03e78c304106bed303411e3470eccc99fb1720ec
Gabriel H. Amaya Pat <g.h.a.p.1.6@gmail.com>
2022-03-20 11:43:26 -0600

    Add Mangum

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+import uvicorn
+
 from fastapi import FastAPI
+from mangum import Mangum
 
 from . import models
 from .api import api_router
@@ -9,3 +12,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(api_router)
+
+handler = Mangum(app)
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=5000)
